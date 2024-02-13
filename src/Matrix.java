ebf4a683a1cae57d169a4a3bdf41087f07a82fc8
Davine Mungai <dmwithiga04@gmail.com>
2024-02-13 13:38:17 -0800

    added junit tests and tried to implement some of the methods for the Matrix class👍

--- src/Matrix.java
+++ src/Matrix.java
@@ -29,37 +29,42 @@ public class Matrix {
     public Matrix (int r, int c) {
 
         // check to see if r and c are positive integers (non-zero)
-        if ((r > 0) || (c>0)) {
+        if ((r > 0) && (c>0)) {
 
             // set mtrx to the correct size and assign parameters to the correct variables
             mtrx = new int[r][c];
             rows = r;
             columns = c;
 
-        }
-
-        else {
-            //TODO: is this retarted? can I just call the 1st constructer intead?
-            // sets the matrix to the default size (2*2)
-            mtrx = new int[2][2];
-
-            // assigns rows and column variable to appropriate number
-            rows = mtrx.length;
-            columns = mtrx[0].length;
-        }
-
-        // sets all elements in mtrx to zero 
-        for (int i = 0; i < mtrx.length; i++) {
-            for (int j = 0; j < mtrx[i].length; j++) {
-                mtrx[i][j] = 0;
+             // sets all elements in mtrx to zero 
+            for (int i = 0; i < mtrx.length; i++) {
+                for (int j = 0; j < mtrx[i].length; j++) {
+                    mtrx[i][j] = 0;
+                }
             }
+
+        }
+
+        // throw if conditions not met
+        else {
+            throw new IllegalArgumentException ("Invalid rows and columns values (values should be positive(non-zero integers)");
         }
 
     }
 
-    //TODO: to create after making the getter and setter method
+    // Makes this Matrix an independent copy of Matrix other
     public Matrix (Matrix other) {
-        
+
+        // set variables in this Matrix in copy of other.
+        mtrx = new int[other.getRows()][getColumns()];
+
+        for (int i = 0; i < mtrx.length; i++) {
+            for (int j = 0; j < mtrx[i].length; j++) {
+                mtrx[i][j] = other.getElement(i, j);
+            }
+        }
+
+
     }
 
 
@@ -79,15 +84,46 @@ public class Matrix {
     public void setElement (int r, int c, int val) {
 
         // check element in position and if r and c are positive (non-zero) numbers
-        if ((r>0||rows>r) || (c>0||rows>c)) {
+        if ((r>0&&r<rows+1) && (c>0&&c>columns+1)) {
             
-            // -1 is becuase of how an array count of elements starts from zero
-            mtrx[r-1][c-1] = val;
+        // -1 is used due to the array index starting at 0
+        mtrx[r-1][c-1] = val;
 
         }
         // throw exception if element position is invalid as per the above conditions
         else{
             throw new IllegalArgumentException ("the element does not exist in the position specified");
         }
-    } 
+    }
+
+    //returns the value of the element in row r and column c
+    public int getElement(int r, int c){
+        // -1 is used due to the array index starting at 0
+        return mtrx[r-1][c-1];
+    }
+
+    //return a String representation of the Matrix class to the specifications of the lab
+    public String toString(){
+        return null;
+    }
+
+    //adds two matrices
+    public Matrix plus(Matrix other){
+        return null;
+    }
+
+    // subtracts two matrices
+    public Matrix minus(Matrix other){
+        return null;
+    }
+
+    //checks if matrix is diagonal
+    public boolean isDiagonal(){
+        return false;
+    }
+
+    //checks if two matraces are equal and returns boolean
+    public static boolean isEqual(Matrix a, Matrix b){
+        return false;
+    }
 }
