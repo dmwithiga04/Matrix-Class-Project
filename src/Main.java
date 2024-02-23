fa93618e6e96dfd2076e873c8a0c283d14a2cc36
Davine Mungai <dmwithiga04@gmail.com>
2024-02-22 20:50:26 -0800

    Implemented more classes and unit tested them🚀🚀

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,11 @@ public class Main {
         Matrix m1 = new Matrix();
         Matrix m2 = new Matrix(2,4);
         
-        m1.setElement(2, 2, 19);
-        m1.setElement(2, 1, 14);
+        m1.setElement(1, 1, 19);
+        m1.setElement(1, 0, 14);
 
-        System.out.println(m1.getElement(2, 1));
-        System.out.println(m1.getElement(2, 2));
 
         Matrix m3 = new Matrix(m1);
-
-        System.out.println(m3.getElement(2, 1));
-        System.out.println(m3.getElement(2, 2));
-
-        m3.setElement(2, 1, 21);
-
-        System.out.println(m3.getElement(2, 1));
     }
     
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,8 +1,10 @@
 //TODO: Add intro template and add Javadocs
 
+import javax.swing.plaf.TreeUI;
+
 public class Matrix {
 
-    //initialize needed variables
+    // initialize needed variables
     private int rows;
     private int columns;
     private int[][] mtrx;
@@ -17,7 +19,7 @@ public class Matrix {
         rows = mtrx.length;
         columns = mtrx[0].length;
 
-        // sets all elements in mtrx to zero 
+        // sets all elements in mtrx to zero
         for (int i = 0; i < mtrx.length; i++) {
             for (int j = 0; j < mtrx[i].length; j++) {
                 mtrx[i][j] = 0;
@@ -26,17 +28,17 @@ public class Matrix {
 
     }
 
-    public Matrix (int r, int c) {
+    public Matrix(int r, int c) {
 
         // check to see if r and c are positive integers (non-zero)
-        if ((r>0) && (c>0)) {
+        if ((r > 0) && (c > 0)) {
 
             // set mtrx to the correct size and assign parameters to the correct variables
             mtrx = new int[r][c];
             rows = r;
             columns = c;
 
-             // sets all elements in mtrx to zero 
+            // sets all elements in mtrx to zero
             for (int i = 0; i < mtrx.length; i++) {
                 for (int j = 0; j < mtrx[i].length; j++) {
                     mtrx[i][j] = 0;
@@ -47,13 +49,14 @@ public class Matrix {
 
         // throw if conditions not met
         else {
-            throw new IllegalArgumentException ("Invalid rows and columns values (values should be positive(non-zero integers)");
+            throw new IllegalArgumentException(
+                    "Invalid rows and columns values (values should be positive(non-zero integers)");
         }
 
     }
 
     // Makes this Matrix an independent copy of Matrix other
-    public Matrix (Matrix other) {
+    public Matrix(Matrix other) {
 
         // set variables in this Matrix in copy of other.
         mtrx = new int[other.getRows()][other.getColumns()];
@@ -63,81 +66,141 @@ public class Matrix {
         for (int i = 0; i < mtrx.length; i++) {
             for (int j = 0; j < mtrx[i].length; j++) {
 
-                // +1 becuase of how the getElement interprates value of rows and columns
-                mtrx[i][j] = other.getElement(i+1, j+1);
+                mtrx[i][j] = other.getElement(i, j);
             }
         }
 
-
     }
-
 
     // Getter and Setter Methods
 
-    //returns number of rows
-    public int getRows(){
+    // returns number of rows
+    public int getRows() {
         return rows;
     }
 
-    //returns number of columns
-    public int getColumns(){
+    // returns number of columns
+    public int getColumns() {
         return columns;
     }
 
-    //assigns an element in a specified location
-    public void setElement (int r, int c, int val) {
+    // assigns an element in a specified location
+    public void setElement(int r, int c, int val) {
 
-        // check element in position and if r and c are positive (non-zero) numbers and an index in the array
-        if ((r>0 && c>0) && (r<=rows && c<= columns))  {
-            
-            // -1 is used due to the array index starting at 0
-            mtrx[r-1][c-1] = val;
+        // check element in position and if r and c are positive numbers and an index in
+        // the array
+        if ((r >= 0 && c >= 0) && (r <= rows - 1 && c <= columns - 1)) {
+
+            // Matirx starting at 0
+            mtrx[r][c] = val;
 
         }
         // throw exception if element position is invalid as per the above conditions
-        else{
-            throw new IllegalArgumentException ("the element does not exist in the position specified");
+        else {
+            throw new IllegalArgumentException("the element does not exist in the position specified");
         }
     }
 
-    //returns the value of the element in row r and column c
-    public int getElement(int r, int c){
-        
-        // check element in position and if r and c are positive (non-zero) numbers
-        if ((r>0) && (c>0) && (r<=rows && c<= columns)) {    
-        
-            // -1 is used due to the array index starting at 0
-            return mtrx[r-1][c-1];
-    
+    // returns the value of the element in row r and column c
+    public int getElement(int r, int c) {
+
+        // check element in position and if r and c are positive numbers
+        if ((r >= 0) && (c >= 0) && (r <= rows - 1 && c <= columns - 1)) {
+
+            // matrix index starting at 0
+            return mtrx[r][c];
+
         }
 
         // throw exception if element position is invalid as per the above conditions
-        else{
-            throw new IllegalArgumentException ("the element does not exist in the position specified");
+        else {
+            throw new IllegalArgumentException("the element does not exist in the position specified");
         }
     }
-    //return a String representation of the Matrix class to the specifications of the lab
-    public String toString(){
+
+    // return a String representation of the Matrix class to the specifications of
+    // the lab
+    public String toString() {
         return null;
     }
 
-    //adds two matrices
-    public Matrix plus(Matrix other){
-        return null;
+    // adds two matrices
+    public Matrix plus(Matrix other) {
+        // checks if both Matrices are of the same dimension
+        if ((rows != other.getRows()) || (columns != other.getColumns())) {
+            throw new IllegalArgumentException("Dimension of matrices not same");
+        }
+
+        // create new matrix object to store the new added values and be returned
+        Matrix resultMatrix = new Matrix(rows, columns);
+
+        // for loop to add both matrices
+        for (int i = 0; i < mtrx.length; i++) {
+            for (int j = 0; j < mtrx[i].length; j++) {
+                // adds elements at the specified position of the two matrices
+                int resultElement = mtrx[i][j] + other.getElement(i, j);
+
+                // inputs resultElement in the resultMatrix at the correct position
+                resultMatrix.setElement(i, j, resultElement);
+
+            }
+        }
+
+        return resultMatrix;
     }
 
     // subtracts two matrices
-    public Matrix minus(Matrix other){
-        return null;
+    public Matrix minus(Matrix other) {
+        // checks if both Matrices are of the same dimension
+        if ((rows != other.getRows()) || (columns != other.getColumns())) {
+            throw new IllegalArgumentException("Dimension of matrices not same");
+        }
+
+        // create new matrix object to store the new subtracts values and be returned
+        Matrix resultMatrix = new Matrix(rows, columns);
+
+        // for loop to subtracts both matrices
+        for (int i = 0; i < mtrx.length; i++) {
+            for (int j = 0; j < mtrx[i].length; j++) {
+                // subtracts elements at the specified position of the two matrices
+                int resultElement = mtrx[i][j] - other.getElement(i, j);
+
+                // inputs resultElement in the resultMatrix at the correct position
+                resultMatrix.setElement(i, j, resultElement);
+
+            }
+        }
+
+        return resultMatrix;
     }
 
-    //checks if matrix is diagonal
-    public boolean isDiagonal(){
+    // checks if matrix is diagonal
+    public boolean isDiagonal() {
         return false;
     }
 
-    //checks if two matraces are equal and returns boolean
-    public static boolean isEqual(Matrix a, Matrix b){
-        return false;
+    // checks if two matraces are equal (dimension and elements) and returns boolean
+    public static boolean isEqual(Matrix a, Matrix b) {
+        // Check Dimension
+        Boolean sameDimension = (a.getRows() == b.getRows()) &&
+                (a.getColumns() == b.getColumns());
+
+        if (sameDimension == false) {
+            return false;
+        }
+
+        // Check Elements if same
+        for (int i = 0; i < a.getRows(); i++) {
+            for (int j = 0; j < a.getColumns(); j++) {
+
+                // Checks if elements at the given position are not the same
+                if (a.getElement(i, j) != b.getElement(i, j)) {
+                    return false;
+                }
+            }
+        }
+
+        // if all checks are true
+        return true;
     }
 }
